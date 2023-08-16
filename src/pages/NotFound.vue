66fc53475c981789b86a3163b8b854166b8b889f
Yusufbek01 <Yusufbek1771@gmail.com>
2023-08-17 01:57:44 +0500

    add NotFound page

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,3 +1,11 @@
 <template>
-  <h1>not found</h1>
+  <section>
+    <base-card>
+      <h2>Page no found</h2>
+      <p>
+        This page could not be found - maybe check out all our
+        <router-link to="/coaches">Couches</router-link>.
+      </p>
+    </base-card>
+  </section>
 </template>
