c9ca3a6834fb5a7465d991ea5d373e7b6682912c
Yusufbek01 <Yusufbek1771@gmail.com>
2023-08-27 00:19:07 +0500

    add signin and signup form

--- src/router.js
+++ src/router.js
@@ -6,6 +6,7 @@ import CoachRegistration from './pages/coaches/CoachRegistration.vue';
 import ContactCoach from './pages/requests/ContactCoach.vue';
 import RequestReceived from './pages/requests/ContactsReceived.vue';
 import NotFound from './pages/NotFound.vue';
+import UserAuth from './pages/auth/UserAuth.vue';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -19,6 +20,7 @@ const router = createRouter({
     ]},
     {path:'/register', component:CoachRegistration},
     {path:'/requests', component:RequestReceived},
+    {path:'/auth', component:UserAuth},
     {path:'/:notFound(.*)', component:NotFound},
   ],
 })
